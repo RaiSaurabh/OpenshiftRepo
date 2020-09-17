307ba6b0eaa216407453d54ae8f78ff23ae6b8f9
Saurabh Rai <rai.saurabh@hotmail.com>
2020-09-17 11:03:35 +0530

    Added Sample Project

---
+++ test-quarkus/src/test/java/com/redhat/NativeTestClassIT.java
@@ -0,0 +1,9 @@
+package com.redhat;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTestClassIT extends TestClassTest {
+
+    // Execute the same tests but in native mode.
+}
